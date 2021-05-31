4d587eefac63791d69403a90a4aecef6e27fb33b
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-31 08:37:26 +0300

    Add member variables

---
+++ src/main/java/Ceaser-Cipher/Cipher.java
@@ -0,0 +1,10 @@
+//class that decrypts the encrypted message
+import java.io.Console;
+
+public class Cipher {
+    //method to rotate the character position
+    private String mText;
+    private int mShift;
+
+    //A constructor
+}
